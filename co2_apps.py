d3c1cdb7a5e46e4f35dacb8289fc9232cc92ab3f
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-06 19:18:30 +0200

    Update co2_apps.py

--- co2_apps.py
+++ co2_apps.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import modelisation
+#import modelisation
 from modelisation import show  
 
 # Chargement des données avec cache
