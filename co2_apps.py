00e0a7ffd4bdb9bdce24934eae783db11ed4d113
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-08 11:14:56 +0200

    remove spaces

--- co2_apps.py
+++ co2_apps.py
@@ -103,16 +103,16 @@ st.title("Projet Emission de CO2")
 
 # Page Accueil (Introduction)
 #if page == "Accueil":
-    st.markdown(""" 
-    ### Introduction
+st.markdown(""" 
+### Introduction
 
-    Le secteur du transport, responsable d'environ 25% des émissions mondiales de dioxyde de carbone (CO2), représente un défi majeur dans la lutte contre le réchauffement climatique. En réponse à l'urgence climatique et aux réglementations de plus en plus strictes, l'industrie automobile doit repenser ses modèles pour réduire l'impact environnemental des véhicules.
+Le secteur du transport, responsable d'environ 25% des émissions mondiales de dioxyde de carbone (CO2), représente un défi majeur dans la lutte contre le réchauffement climatique. En réponse à l'urgence climatique et aux réglementations de plus en plus strictes, l'industrie automobile doit repenser ses modèles pour réduire l'impact environnemental des véhicules.
 
-    L'analyse de données joue un rôle crucial dans ce processus. En explorant les caractéristiques techniques des véhicules, nous pouvons non seulement comprendre les facteurs influençant les émissions de CO2, mais aussi anticiper les émissions des futurs modèles. Grâce à des approches statistiques avancées et à la modélisation prédictive, nous pouvons identifier des leviers de réduction des émissions et proposer des solutions adaptées pour une mobilité plus durable.
+L'analyse de données joue un rôle crucial dans ce processus. En explorant les caractéristiques techniques des véhicules, nous pouvons non seulement comprendre les facteurs influençant les émissions de CO2, mais aussi anticiper les émissions des futurs modèles. Grâce à des approches statistiques avancées et à la modélisation prédictive, nous pouvons identifier des leviers de réduction des émissions et proposer des solutions adaptées pour une mobilité plus durable.
 
-    ### Objectifs
+### Objectifs
 
-    L'objectif de ce projet est d'appliquer des techniques d'analyse de données pour étudier les émissions de CO2 des véhicules. En utilisant des jeux de données détaillant les caractéristiques techniques de nombreux modèles, nous allons :
+L'objectif de ce projet est d'appliquer des techniques d'analyse de données pour étudier les émissions de CO2 des véhicules. En utilisant des jeux de données détaillant les caractéristiques techniques de nombreux modèles, nous allons :
 
     - Mener une analyse exploratoire pour identifier les relations entre les caractéristiques des véhicules et leurs émissions.
     
@@ -120,8 +120,8 @@ st.title("Projet Emission de CO2")
 
     - Proposer des recommandations pratiques pour réduire les émissions de CO2, en s'appuyant sur les résultats de nos analyses.
 
-    À travers ce projet, nous visons à mieux comprendre les dynamiques sous-jacentes aux émissions de CO2 et à apporter des contributions significatives pour une mobilité plus verte et durable.
-    """)
+À travers ce projet, nous visons à mieux comprendre les dynamiques sous-jacentes aux émissions de CO2 et à apporter des contributions significatives pour une mobilité plus verte et durable.
+""")
 
 # Page Exploration
 #if page == "Exploration":
