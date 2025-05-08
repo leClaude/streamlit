172f5bdb304d23363e74e0cfcaf7080f09a1d983
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-08 11:38:33 +0200

    changement page accueil

--- co2_apps.py
+++ co2_apps.py
@@ -1,9 +1,13 @@
-# Accueil
 import streamlit as st
+import streamlit_extras.switch_page as sp
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
+
+
+# Rediriger vers la page d'accueil au lancement
+sp.switch_page("pages/accueil.py") 
 
 # Chargement des données avec cache
 @st.cache_data
@@ -92,49 +96,4 @@ df_eu_clean = preprocess_eu(df_europe)
 # Envoi des données nettoyées à la fonction d'affichage
 #modelisation.show(df_fr_clean, df_eu_clean)
 
-st.title("Projet Emission de CO2")
 
-#st.sidebar.title('Sommaire')
-
-# Barre de navigation
-#page = st.sidebar.selectbox(
-#    "Navigation",
-#    ["Accueil","Exploration", "Data Visualisation", "Modelisation"]
-#)
-
-# Page Accueil (Introduction)
-#if page == "Accueil":
-st.markdown(""" 
-### Introduction
-
-Le secteur du transport, responsable d'environ 25% des émissions mondiales de dioxyde de carbone (CO2), représente un défi majeur dans la lutte contre le réchauffement climatique. En réponse à l'urgence climatique et aux réglementations de plus en plus strictes, l'industrie automobile doit repenser ses modèles pour réduire l'impact environnemental des véhicules.
-
-L'analyse de données joue un rôle crucial dans ce processus. En explorant les caractéristiques techniques des véhicules, nous pouvons non seulement comprendre les facteurs influençant les émissions de CO2, mais aussi anticiper les émissions des futurs modèles. Grâce à des approches statistiques avancées et à la modélisation prédictive, nous pouvons identifier des leviers de réduction des émissions et proposer des solutions adaptées pour une mobilité plus durable.
-
-### Objectifs
-
-L'objectif de ce projet est d'appliquer des techniques d'analyse de données pour étudier les émissions de CO2 des véhicules. En utilisant des jeux de données détaillant les caractéristiques techniques de nombreux modèles, nous allons :
-
-    - Mener une analyse exploratoire pour identifier les relations entre les caractéristiques des véhicules et leurs émissions.
-    
-    - Utiliser des modèles prédictifs pour estimer les émissions des véhicules futurs en fonction de leurs spécifications techniques.
-
-    - Proposer des recommandations pratiques pour réduire les émissions de CO2, en s'appuyant sur les résultats de nos analyses.
-
-À travers ce projet, nous visons à mieux comprendre les dynamiques sous-jacentes aux émissions de CO2 et à apporter des contributions significatives pour une mobilité plus verte et durable.
-""")
-
-# Page Exploration
-#if page == "Exploration":
-#    import exploration as exploration
-#    exploration.show()
-
-# Page Data Visualisation
-#elif page == "Data Visualisation":
-#    import dataviz
-#    dataviz.show()
-
-# Page Modélisation
-#elif page == "Modélisation":
-#    import modelisation
-#    modelisation.show(df_fr_clean, df_eu_clean)
