101ed0c6a411775ea590ec7ff0148ff915ce5a1f
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-08 11:57:01 +0200

    requirements

--- co2_apps.py
+++ co2_apps.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_extras.switch_page as sp
+from streamlit_extras.switch_page_button import switch_page as sp
 
 
 # Rediriger vers la page d'accueil au lancement
