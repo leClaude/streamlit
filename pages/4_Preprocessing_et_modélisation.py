044551db3b2052dc33c9e626a6ffda596379b119
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-11 17:02:16 +0200

    Update 4_Preprocessing_et_modélisation.py

--- pages/4_Preprocessing_et_modélisation.py
+++ pages/4_Preprocessing_et_modélisation.py
@@ -215,20 +215,13 @@ def load_pipeline(_gbr,_preprocessor):
 model_pipeline = load_pipeline(gbr,preprocessor)
 
 # Perform GridSearchCV to find the best hyperparameters
-import threading
-import pickle
 
 def train_model():
     grid_search = GridSearchCV(estimator=model_pipeline, param_grid=param_grid, cv=5, n_jobs=-1, verbose=1, scoring='neg_mean_squared_error')
     grid_search.fit(X_train, y_train)
-    
-    with open("grid_search.pkl", "wb") as f:
-        pickle.dump(grid_search, f)  # Sauvegarde du modèle
     return grid_search
 
-thread = threading.Thread(target=train_model)
-thread.start()
-thread.join()
+grid_search = train_model()
 
 # Charger le modèle sauvegardé
 try:
