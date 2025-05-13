4d54ebbe4f18c65608a477c6532e269a58ffaa77
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-13 15:36:04 +0200

    Update 4_Preprocessing_et_modélisation.py

--- pages/4_Preprocessing_et_modélisation.py
+++ pages/4_Preprocessing_et_modélisation.py
@@ -295,7 +295,7 @@ def importance_graph():
     plt.barh(feat_importances.index, feat_importances['Importance'])
     plt.title("Importance")
     plt.ylabel(feat_importances.index)
-    plt.invert_yaxis()
+    plt.yticks(rotation=0)
     plt.xlabel("indicateur")
     
     return fig
