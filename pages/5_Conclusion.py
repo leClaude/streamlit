7736e5760a715d76bd1879a6dc0f0eac53a7d939
leClaude <32574221+leClaude@users.noreply.github.com>
2025-05-22 16:39:46 +0200

    Update 5_Conclusion.py

--- pages/5_Conclusion.py
+++ pages/5_Conclusion.py
@@ -8,12 +8,12 @@ st.markdown("""
     Notre objectif était d’identifier les principaux facteurs influençant les émissions de CO₂ des véhicules, à l’aide de modèles de machine learning appliqués à deux jeux de données : celui de l’ADEME pour le marché français, et celui de l’Agence Européenne de l’Environnement pour une vision à l’échelle européenne.
 Côté modélisation, le Gradient Boosting Regressor a donné les meilleurs résultats, avec un score R² allant jusqu’à 0.965 sur les données ADEME et 0.925 sur le dataset européen avec des variables clés comme la masse, la puissance, la cylindrée et le type de carburant.
 
-# Limites
+### Limites
 - Fort déséquilibre du dataset : >80 % de minibus.
 - Surreprésentation de certains constructeurs.
 - Difficulté à généraliser les résultats à l’ensemble du parc automobile
 
-# Pistes à explorer
+### Pistes à explorer
 - Rééquilibrer ou enrichir le dataset ADEME avec des véhicules plus variés.
 - Explorer la fusion de datasets pour améliorer la représentativité.
 - Alléger le modèle en supprimant les variables les moins influentes, pour gagner en simplicité et en performance.
